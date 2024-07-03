c913f62f638e21f5a71884d5738e051af97f46b9
Arun Prasaad <arunbear@cpan.org>
2024-07-03 13:59:56 +0100

    Make test parameterized

--- leapyears/src/test/java/org/example/LeapYearCalculatorSpec.java
+++ leapyears/src/test/java/org/example/LeapYearCalculatorSpec.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.IndicativeSentencesGeneration;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.example.LeapYearCalculator.isLeapYear;
 
@@ -13,9 +16,10 @@ class LeapYearCalculatorSpec {
     @IndicativeSentencesGeneration(separator = " -> ", generator = DisplayNameGenerator.ReplaceUnderscores.class)
     class A_year_is_a_leap_year {
 
-        @Test
-        void if_it_is_divisible_by_4_but_not_by_100() {
-            assertThat(isLeapYear(2004)).isTrue();
+        @ParameterizedTest
+        @ValueSource(ints = { 2004, 1984, 4 })
+        void if_it_is_divisible_by_4_but_not_by_100(int year) {
+            assertThat(isLeapYear(year)).isTrue();
         }
 
     }
